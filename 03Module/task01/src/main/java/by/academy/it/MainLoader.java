cd880ed7ce8eacb2d2ada43327731bf5ba4afa97
insrchofsnrs <e.vavilkin@gmail.com>
2017-10-23 21:22:26 +0300

    firs launch firs life

---
+++ 03Module/task01/src/main/java/by/academy/it/MainLoader.java
@@ -0,0 +1,12 @@
+package by.academy.it;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainLoader {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Context.xml");
+        Pet pet = context.getBean("pet", Pet.class);
+        System.out.println(pet.getPerson().getName());
+    }
+}
---
+++ 03Module/task01/src/main/java/by/academy/it/Pet.java
@@ -0,0 +1,23 @@
+package by.academy.it;
+
+public class Pet {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    Person person;
+}
