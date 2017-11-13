ec0e7c0d0e67aaecdc7a3864c14b01ba474f76d0
insrchofsnrs <e.vavilkin@gmail.com>
2017-11-14 00:25:50 +0300

    hibernate task24 2%

---
+++ 02Module/task24/src/main/java/by/academy/it/EMUtil.java
@@ -0,0 +1,38 @@
+package by.academy.it;
+
+import org.hibernate.SessionFactory;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EMUtil {
+
+    private static final EntityManagerFactory emFactory;
+    private static final EntityManagerFactory emFactoryTest;
+
+
+    // EMF
+    static {
+        emFactory = Persistence.createEntityManagerFactory("by.academy.it");
+    }
+
+    //Test EMF
+    static {
+        emFactoryTest = Persistence.createEntityManagerFactory("by.academy.it.test");
+    }
+
+    public static EntityManager getEntityManager() {
+        return emFactory.createEntityManager();
+    }
+
+    public static EntityManager getTestEntityManager() {
+        return emFactoryTest.createEntityManager();
+    }
+
+    public static void close() {
+        emFactory.close();
+        emFactoryTest.close();
+
+    }
+}
---
+++ 02Module/task24/src/test/java/by/academy/it/Tests.java
@@ -0,0 +1,4 @@
+package by.academy.it;
+
+public class Tests {
+}
